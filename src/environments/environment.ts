0bdf259c362140d5e3f76e7ae8dada175de465c8
reynave <felix.renaldi@gmail.com>
2022-11-02 15:30:30 +0700

    add cordova

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@ declare var env_socket : any;
 
 export const environment = {
   production: false,
-  api: "http://localhost/application/cso/api/", 
-  base_url: "#/",
-  version : "1.1b",
-  token : 'DEV-WEBBASE-15fc3cec-5b65-4fb9-95af-r53bef239228',
-  socket_url : 'http://localhost:3000',
-  // api: env_api, 
+  // api: "http://localhost/application/cso/api/", 
   // base_url: "#/",
-  // version : "1.1",
+  // version : "1.1b",
   // token : 'DEV-WEBBASE-15fc3cec-5b65-4fb9-95af-r53bef239228',
-  // socket_url : env_socket,
+  // socket_url : 'http://localhost:3000',
+  api: env_api, 
+  base_url: "#/",
+  version : "1.1",
+  token : 'DEV-WEBBASE-15fc3cec-5b65-4fb9-95af-r53bef239228',
+  socket_url : env_socket,
 };
  
